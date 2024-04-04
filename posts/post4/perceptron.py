4c12589a8d9ed4a560a87216446c00d62d181d0a
lfschweitzer <144742431+lfschweitzer@users.noreply.github.com>
2024-04-04 11:57:31 -0400

    addressed comments for E

--- posts/post4/perceptron.py
+++ posts/post4/perceptron.py
@@ -57,27 +57,49 @@ class Perceptron(LinearModel):
             that the final column of X is a constant column of 1s. 
 
             y, torch.Tensor: the target vector.  y.size() = (n,). In the perceptron algorithm, the possible labels for y are assumed to be {-1, 1}
+        
+        RETURNS: 
+            loss: float: the loss value of the model
         """
         
         y_hat = self.predict(X)
-        #if y_hat=0=>-1, if y_hat=1=>1
+        
+        # convert y_hat from {0, 1} to {-1, 1}
         y_hat = 2*y_hat - 1
+        
         misc = 1.0*(y_hat*y <= 0)
-                
+                        
         return misc.mean()
 
     def grad(self, X, y):
-        # should correctly return the “update” part of the perceptron update
+        """
+        Should compute the gradient of the empirical risk
         
+        ARGUMENTS: 
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+            y, torch.Tensor: the target vector.  y.size() = (n,). The possible labels for y are {0, 1}
+
+        RETURNS:
+            grad: float: the gradient of the empirical risk of the model
+        """
         s = self.score(X)
+        
+        # choose random learning rate
+        learning_rate = 0.01
 
-       # if misclassified, calculate update
-        if s*y <= 0:            
-            update_val = X*y
-            return update_val[0,:]
-        else:
-            return torch.zeros_like(self.w)
-
+        # if misclassified, calculate update
+        misclass = s*y <= 0
+        update_val_row = X*y[:,None]
+        
+        update_val = update_val_row * misclass[:,None]
+        
+        r = learning_rate * torch.mean(update_val, 0)
+        
+        return r
+       
 class PerceptronOptimizer:
 
     def __init__(self, model):
@@ -86,10 +108,22 @@ class PerceptronOptimizer:
     
     def step(self, X, y):
         """
-        Compute one step of the perceptron update using the feature matrix X 
+        Compute one step of the update using the feature matrix X 
         and target vector y. 
+        
+        ARGUMENTS: 
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+            y, torch.Tensor: the target vector.  y.size() = (n,). The possible labels for y are {0, 1}
+            alpha: float: learning rate of model
+            beta: float: momentum of model
+        RETURNS:
+            loss: float: the empirical risk
         """
         loss = self.model.loss(X, y)
+        
         grad = self.model.grad(X, y)
         self.model.w += grad
         
